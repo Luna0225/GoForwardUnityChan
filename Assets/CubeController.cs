27b672dfa75236d55f5b51fd1d3a0871ce155b39
Luna0225 <132825841+Luna0225@users.noreply.github.com>
2023-07-04 00:24:11 +0900

    キューブまたは地面に接触したときに音を鳴らすように変更

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -10,10 +10,18 @@ public class CubeController : MonoBehaviour
 
     //消滅位置
     private float deadLine = -10;
+
+
+    //seを呼び出す用
+    public AudioClip audioClip;
+
+    public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //AudioSourceを取得
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,9 +31,28 @@ public class CubeController : MonoBehaviour
         transform.Translate(this.speed * Time.deltaTime, 0, 0);
 
         //画面外に出たら消滅させる
-        if(transform.position.x < this.deadLine)
+        if (transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
+        }
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //キューブまたは地面に当たった時に効果音を出す 接触判定
+        if (collision.gameObject.CompareTag("CubeTag") || collision.gameObject.CompareTag("GroundTag"))
+        {
+            //seを鳴らす
+            audioSource.PlayOneShot(audioClip);
+            //ログの表示
+            Debug.Log("接触");
+        }
+        else
+        {
+            //ログの表示
+            Debug.Log("別のオブジェクトに接触");
         }
     }
 }
